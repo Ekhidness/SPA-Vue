c5b5bab8f963f6452524694270f076b1293a644f
Ekhidna <ekhidna@disroot.org>
2026-03-11 01:43:17 +0700

    Template for login component

---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <form class="login" @submit.prevent="login">
+    <h1>Sign in</h1>
+    <label>User name</label>
+    <input type="text" required v-model="username" />
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+    <hr />
+    <button type="submit">Login</button>
+  </form>
+</template>
